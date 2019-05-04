f85b39ce5fd873871afb5a818d6dfb45a9a4099c
Arina Merculov <merculova1997@mail.ru>
2019-05-04 14:22:04 +0300

    Add project files.

---
+++ FloriProject.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FloriProject.Web.Startup))]
+namespace FloriProject.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
